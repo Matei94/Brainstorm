378af15c325040df2284892a807504d00dbec5eb
Răzvan-Mădălin MATEI <matei.rm94@gmail.com>
2015-11-30 14:48:41 +0200

    collapsable chat

--- public/js/index.js
+++ public/js/index.js
@@ -1,6 +1,14 @@
 /*** DOCUMENT READY **********************************************************/
 
 $(document).ready(function() {
+  $(".header").click(function () {
+    $header = $(this);
+    //getting the next element
+    $content = $header.next();
+    //open up the content needed - toggle the slide- if visible, slide up, if not slidedown.
+    $content.slideToggle(500);
+  });
+
   $.get("/session", function(data) {
     var messagesRef = new Firebase('https://matei.firebaseio.com/' + data);
 
