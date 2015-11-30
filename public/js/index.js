c3bce833663c10791b7055d0477a75b5a922415e
Alexandru Florea <alex.florea0106@gmail.com>
2015-11-30 15:49:54 +0200

    textEditor working?

--- public/js/index.js
+++ public/js/index.js
@@ -11,17 +11,6 @@ var textEditorHash;
 
 $(document).ready(function() {
   console.log(window.location.href);
-
-  var firepadRef = getExampleRef();
-  var codeMirror = CodeMirror(document.getElementById('feature'), { lineWrapping: true });
-  var firepad = Firepad.fromCodeMirror(firepadRef, codeMirror,
-  { richTextToolbar: true, richTextShortcuts: true });
-  firepad.on('ready', function() {
-    if (firepad.isHistoryEmpty()) {
-      firepad.setHtml('<span style="font-size: 24px;">Rich-text editing with <span style="color: red">Firepad!</span></span><br/><br/>Collaborative-editing made easy.\n');
-    }
-  });
-
   collapseChat();
 
   $.get("/session", function(data) {
@@ -94,6 +83,21 @@ function collapseChat() {
   $(".chat-header").click(function() {
     $(".chat-content").slideToggle(500);
   });
+}
+
+function showTextEditor () {
+    $(".tab1").click(function() {
+        var firepadRef = getExampleRef()
+        var codeMirror = CodeMirror(document.getElementById('feature'), { lineWrapping: true });
+        var firepad = Firepad.fromCodeMirror(firepadRef, codeMirror,
+        { richTextToolbar: true, richTextShortcuts: true });
+        firepad.on('ready', function() {
+            if (firepad.isHistoryEmpty()) {
+                firepad.setHtml('<span style="font-size: 24px;">Rich-text editing with <span style="color: red">Firepad!</span></span><br/><br/>Collaborative-editing made easy.\n');
+            }
+        });
+        
+    });
 }
 
 /*****************************************************************************/
